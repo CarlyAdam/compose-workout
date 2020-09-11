d32df9982db36cd3bbf7b8da31483927b13f6f89
Adam <carlyadam88@gmail.com>
2020-09-11 13:34:31 -0500

    created compose observers

--- app/src/main/java/com/carlyadam/compose/ui/MainActivity.kt
+++ app/src/main/java/com/carlyadam/compose/ui/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.carlyadam.compose.ui
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.ui.platform.setContent
-import androidx.lifecycle.Observer
-import com.carlyadam.compose.data.states.UserState
 import com.carlyadam.compose.ui.composables.UsersListUi
 import com.carlyadam.compose.utils.Coroutines
 import com.carlyadam.compose.viewmodel.UserViewModel
@@ -15,22 +12,14 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private val usersState: UserState = UserState()
-
     private val usersViewModel: UserViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        usersViewModel.responseLiveData.observe(this, Observer {
-            usersState.users.addAll(it)
-            setContent {
-                UsersListUi.addList(usersState)
-            }
-        })
-        usersViewModel.errorLiveData.observe(this, Observer {
-            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
-        })
+        setContent {
+            UsersListUi.addList(usersViewModel)
+        }
 
         getData()
     }
--- app/src/main/java/com/carlyadam/compose/ui/composables/UserListComposable.kt
+++ app/src/main/java/com/carlyadam/compose/ui/composables/UserListComposable.kt
@@ -1,32 +1,55 @@
 package com.carlyadam.compose.ui.composables
 
+import android.widget.Toast
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import com.carlyadam.compose.data.states.UserState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.platform.ContextAmbient
+import com.carlyadam.compose.data.model.User
+import com.carlyadam.compose.viewmodel.UserViewModel
 
 
 object UsersListUi {
 
     @Composable
-    fun addList(state: UserState) {
+    fun addList(userViewModel: UserViewModel) {
+        val response by userViewModel.responseLiveData.observeAsState()
+        val error by userViewModel.errorLiveData.observeAsState()
+
+        response?.let {
+           showUsers(it)
+        }
+
+        error?.let {
+            showError(error!!)
+        }
+    }
+
+    @Composable
+    fun showUsers(usersList: List<User>) {
         MaterialTheme {
             Column {
-                state.users.forEach {
+                usersList.forEach {
                     Column {
                         Row {
                             Text(text = it.name)
                         }
                         Text(text = it.lastname)
-
                     }
 
                 }
             }
         }
+    }
 
+    @Composable
+    fun showError(error: String) {
+        val context = ContextAmbient.current
+        Toast.makeText(context,error,Toast.LENGTH_SHORT).show()
     }
 
 
